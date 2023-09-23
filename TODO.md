e8efcd0cb320e99f5d09cce036f4efc930c15a2c
nachomglz <nachopad3@gmail.com>
2023-09-23 20:26:05 +0200

    Send HTTP 200 response

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Todo
+
+- [x] Create tcp server that listens to tcp connections
+- [ ] Serve static files placed in the static/ directory
+
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::fs::File;
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::io::Write;
+use std::net::{TcpListener, TcpStream, Shutdown};
 
-fn handle_request(mut stream: TcpStream) {
-
+fn handle_request(_stream: &TcpStream) -> String {
+    "HTTP/1.1 200 OK\r\n\r\nEverything worked as expected".to_owned()
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        println!("Connection stablished!");
+        let response = handle_request(&stream);
+
+        stream.write(response.as_bytes())?;
+
+        // Close connection stream
+        stream.shutdown(Shutdown::Both).expect("Failed to shutdown connection stream");
     }
+    Ok(())
 }
