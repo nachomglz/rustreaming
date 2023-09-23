a08792c512230c8954761a0393ca9c577496199e
nachomglz <nachopad3@gmail.com>
2023-09-23 18:40:38 +0200

    create tcp server that listens to tcp connections

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a Streaming server written in Rust.
 ## Why?
 
 I'm doing this project to learn some networking and Rust at the same time.
+
+## What is the goal?
+
+Write an streaming service from scratch on a TCP server to demonstrate networking skills.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_request(mut stream: TcpStream) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        println!("Connection stablished!");
+    }
 }
